3264880b67b56bcba563579beb8fd650f9867296
abrormukimov <abrormm34@gmail.com>
2020-06-11 20:39:23 +0500

    Add tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+  	self.tags.collect do |tag|
+  		tag.name
+  	end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+  	tags_string = ["tag1", "tag2", "tag3"]
+  end
 end
