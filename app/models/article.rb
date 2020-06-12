beacf8562cdc28e2c439516a0585aa6e277c28b8
abrormukimov <abrormm34@gmail.com>
2020-06-12 13:48:45 +0500

    Modify taggin\g

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,8 @@ class Article < ApplicationRecord
   end
 
   def tag_list=(tags_string)
-  	tags_string = ["tag1", "tag2", "tag3"]
-  end
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+end
 end
